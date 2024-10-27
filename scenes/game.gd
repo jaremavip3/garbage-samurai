bd394b982f85526b732932b5fe4bbc8084447b30
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2024-10-27 00:18:34 -0400

    Revert "Merge branch 'main' of https://github.com/jaremavip3/Garbage-Samurai"
    This reverts commit 1fdcf432f4e0a8f550bcf7a98bf2525c880aeff8, reversing
    changes made to ff0b1609a8bcc944300a159458128f53a152aab8.

--- scenes/game.gd
+++ scenes/game.gd
@@ -5,7 +5,6 @@ var rand_y
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#spawn the obstacles
 	randomize()
 	for i in range(50):
 		var tree = preload("res://scenes/Tree.tscn").instantiate()
@@ -23,7 +22,6 @@ func _ready() -> void:
 		rock.global_position = Vector2(rand_x, rand_y)
 		add_child(rock)
 		
-<<<<<<< Updated upstream
 	randomize()
 	for i in range(40):
 		var volcano_tree = preload("res://scenes/VolcanoTree.tscn").instantiate()
@@ -40,16 +38,6 @@ func _ready() -> void:
 		cactus.global_position = Vector2(rand_x, rand_y)
 		add_child(cactus)
 		
-=======
-	#spawn the trash
-	var total_trash = randi_range(10, 50)
-	for i in total_trash:
-		var trash = preload("res://scenes/trash.tscn").instantiate()
-		rand_x = randi_range(-200, 200)
-		rand_y = randi_range(-200, 200)
-		trash.global_position = Vector2(rand_x, rand_y)
-		add_child(trash)
->>>>>>> Stashed changes
 		
 	
 
--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -1,10 +1,6 @@
 extends Node
 
 var score = 0
-<<<<<<< Updated upstream
-=======
-@onready var score_label: Label = $ScoreLabel
->>>>>>> Stashed changes
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,8 +13,5 @@ func _process(delta: float) -> void:
 	
 func add_point():
 	score += 1 
-	score_label.text = "Your score is " + str(score)
+	score_label.text = "Collected: " + str(score)
 	
-
-func collect_trash():
-	print("no")
